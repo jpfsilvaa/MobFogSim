faa5cc6ddeb962af36bd27ab6c1937eda001a23b
jps <j263792@dav.unicamp.br>
2021-11-24 22:57:27 -0300

    It was finded a way to access the characteristics (CPU, RAM, ...) from the cloudlet and from the smart thing. For this, the Facade pattern was used. Then the simulator complex interactions stays masked from the optimization step.

--- src/org/fog/optimization/Allocation.java
+++ src/org/fog/optimization/Allocation.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.fog.entities.FogDevice;
 import org.fog.entities.MobileDevice;
+import org.fog.optimization.facade.DeviceCharacsFacade;
 import org.fog.vmmigration.Migration;
 
 public class Allocation {
@@ -17,8 +18,8 @@ public class Allocation {
 	/**
 	 * @author jps
 	 * 
-	 * @param (WORKING) cloudlets available in distance
-	 * @param (WORKING) smartThings requesting resources
+	 * @param cloudlets available in distance
+	 * @param smartThings requesting resources
 	 * @return (WORKING) one cloudlet id 
 	 * 
 	 * (in the future, must return a set of cloudlet ids for all smartThing requesting)
@@ -26,12 +27,13 @@ public class Allocation {
 	public int calculateAllocations(List<FogDevice> cloudlets, List<MobileDevice> smartThings) {
 		System.out.printf("%s: calculateAllocation%n", TAG);
 		
-		int cloudletResultId = 0;
+		DeviceCharacsFacade facade = DeviceCharacsFacade.getInstance();
+		int cloudletResultId = 0;		
 		
-		// smartThings.get(0).getCharacteristics()
-		
-		if (cloudlets.get(0).getRatePerMips() >= smartThings.get(0).getRatePerMips()) {
-			cloudletResultId = cloudlets.get(0).getId();
+		if (facade.getRAM(cloudlets.get(0)) >= facade.getRAM(smartThings.get(0))) {
+			System.out.printf("%s cloudlet %d allocated%n", TAG, cloudlets.get(0).getMyId());
+			
+			cloudletResultId = cloudlets.get(0).getMyId();
 		}
 		
 		System.out.printf("%s: calculateAllocation - result: %d%n", TAG, cloudletResultId);
